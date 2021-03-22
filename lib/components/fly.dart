0ffbf7ad9e99ebfe2e64203932b2564e2fa22d2c
Lightbpk <bpk735@gmail.com>
2021-03-22 20:31:18 +0700

    horizontal_dvizh

--- lib/components/fly.dart
+++ lib/components/fly.dart
@@ -11,8 +11,11 @@ class Fly {
   List<Sprite> flyingSprite;
   Sprite deadSprite;
   double flyingSpriteIndex = 0;
+  Offset targetLocation;
 
-  Fly(this.game);
+  Fly(this.game){
+    setTargetLocation();
+  }
   void render(Canvas c){
     if(isDead){
       deadSprite.renderRect(c, flyRect.inflate(2));
@@ -23,19 +26,38 @@ class Fly {
   void update(double t){
     if(isDead){
       flyRect = flyRect.translate(0, game.tileSize * 12 * t);
+      if(flyRect.top > game.screenSize.height){
+        isOffScreen = true;
+        print('Fly fly away');
+      }
     }else {
       flyingSpriteIndex += 30 * t;//МАГИЯ анимации предполагается что t = 0,016
       if (flyingSpriteIndex >= 2){
         flyingSpriteIndex -= 2;
       }
-    }
-    if(flyRect.top > game.screenSize.height){
-      isOffScreen = true;
-      print('Fly fly away');
+      //------------------ как это работает толком не понятно
+      double stepDistance = speed * t;
+      Offset toTarget = targetLocation - Offset(flyRect.left,flyRect.top);
+      if (stepDistance < toTarget.distance){
+        Offset stepToTarget = Offset.fromDirection(toTarget.direction,stepDistance);
+        flyRect = flyRect.shift(stepToTarget);
+      }else {
+        flyRect = flyRect.shift(toTarget);
+        setTargetLocation();
+      }
+      //------------------
+
     }
   }
   void onTapDown(){
     isDead = true;
     game.spawnFly();
+  }
+  double get speed => game.tileSize * 3 ;
+
+  void setTargetLocation() {
+    double x = game.rnd.nextDouble() * (game.screenSize.width - (game.tileSize * 2.025));
+    double y = game.rnd.nextDouble() * (game.screenSize.height - (game.tileSize * 2.025));
+    targetLocation = Offset(x,y);
   }
 }
